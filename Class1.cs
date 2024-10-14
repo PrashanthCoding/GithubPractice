cbaa428cb932b7b5e9ddf084c4e66f98020e7b73
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-14 16:23:12 +0530

    Class1.css

--- Class1.cs
+++ Class1.cs
@@ -4,14 +4,16 @@ public class Class1
 {
     public Class1()
     {
-        static void MyMethod() 
+        static void MyMethod()
         {
             Console.WriteLine("I just got executed!");
         }
-        
+
         static void Main(string[] args)
         {
-           MyMethod();
+            MyMethod();
+            MyMethod();
+            MyMethod();
         }
     }
 }
