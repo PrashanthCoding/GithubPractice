f7c56f4eff1cdf3fa220110d854c4f62f1bf5752
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-14 14:59:20 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ public class Class1
         Console.WriteLine("Enter a input values: ");
         var input = Console.ReadLine();
         var reverse = "";
-        int i = input.Length -1;
-        
-        while(i >=0)
+        int i = input.Length - 1;
+
+        while (i >= 0)
         {
             reverse += input[i];
             i--;
         }
 
-        if(input == reverse)
+        if (input == reverse)
         {
             Console.WriteLine("The Input is a  palindrome.");
         }
