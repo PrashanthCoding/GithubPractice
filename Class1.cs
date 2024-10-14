1a6e89e3aba82306b9eebde3b78cb37f9d219a62
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-14 16:19:49 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -6,16 +6,10 @@ public class Class1
     {
         static void Main(string[] args)
         {
-            int myNum = 5;
-            double myDoubleNum = 5.99D;
-            char myLetter = 'D';
-            bool myBool = true;
-            string myText = "Hello";
-            Console.WriteLine(myNum);
-            Console.WriteLine(myDoubleNum);
-            Console.WriteLine(myLetter);
-            Console.WriteLine(myBool);
-            Console.WriteLine(myText);
+            string firstName = "John ";
+            string lastName = "Doe";
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName);
         }
     }
 }
