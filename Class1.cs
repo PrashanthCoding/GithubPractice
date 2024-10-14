86d08f5eb30fe3503b5c6c9642b3d6a997d46990
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-14 16:17:01 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -6,6 +6,11 @@ public class Class1
     {
         static void Main(string[] args)
         {
+            int myNum = 5;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
             Console.WriteLine("Hello World!");
         }
     }
