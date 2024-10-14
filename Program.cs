7f37263b42171d36d5ca29a55ebaab611d521f59
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-14 12:58:26 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -6,5 +6,7 @@ public class Class1
     {
         // This is Class1 
         Console.WriteLine("Hello World...!");
+
+        // Standardization: Using standardized systems, packaging, and practices across the supply chain can help mitigate incompatibility.
     }
 }
