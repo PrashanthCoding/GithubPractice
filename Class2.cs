9fe151fa0a2a6636fdd178030a29cee89f76fc69
PrashanthCoding <prashanth100076@gmail.com>
2024-10-14 19:03:04 +0530

    Class2.cs Added

---
+++ Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        int i;
+        Console.Write("Enter a Number : ");
+        i = int.Parse(Console.ReadLine());
+        if (i % 2 == 0)
+        {
+            Console.Write("Entered Number is an Even Number");
+            Console.Read();
+        }
+        else
+        {
+            Console.Write("Entered Number is an Odd Number");
+            Console.Read();
+        }
+
+    }
+}
