32890a2b5316a2857df441dc3f7cf4cf68a0aa52
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-14 14:11:34 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -5,18 +5,6 @@ public class Class1
     // Declare a Class1.
     public Class1()
     {
-        // This is Class1 
         Console.WriteLine("Hello World...!");
-
-        // Standardization: Using standardized systems, packaging, and practices across the supply chain can help mitigate incompatibility.
-
-        //Interoperability: Ensuring that software systems can communicate with one another (through APIs or middleware) is critical.
-
-        // Collaboration: Building strong relationships and clear communication with partners helps align different logistical practices.
-
-        // Regulatory Alignment: Staying informed about international regulatory requirements and adapting processes to meet them.
-
-
-        // Incompatibility in logistics is a common challenge but can be addressed through proper planning, technology integration, and coordination between stakeholders.
     }
 }
