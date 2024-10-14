cba8860aed0faf5d7981ca16c0b89042b5d5f35b
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-14 16:31:50 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,36 +1,24 @@
-﻿using System;
-
-public class Class1
+// Interface
+interface IAnimal 
 {
-    // Abstract class
-    abstract class Animal
-    {
-        // Abstract method (does not have a body)
-        public abstract void animalSound();
-        // Regular method
-        public void sleep()
-        {
-            Console.WriteLine("Zzz");
-        }
-    }
+  void animalSound(); // interface method (does not have a body)
+}
 
-    // Derived class (inherit from Animal)
-    class Pig : Animal
-    {
-        public override void animalSound()
-        {
-            // The body of animalSound() is provided here
-            Console.WriteLine("The pig says: wee wee");
-        }
-    }
+// Pig "implements" the IAnimal interface
+class Pig : IAnimal 
+{
+  public void animalSound() 
+  {
+    // The body of animalSound() is provided here
+    Console.WriteLine("The pig says: wee wee");
+  }
+}
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Pig myPig = new Pig(); // Create a Pig object
-            myPig.animalSound();  // Call the abstract method
-            myPig.sleep();  // Call the regular method
-        }
-    }
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    Pig myPig = new Pig();  // Create a Pig object
+    myPig.animalSound();
+  }
 }
