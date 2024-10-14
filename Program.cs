f1c966c14dba6117a0334b4f3bcd19cfbf097960
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-14 13:02:47 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -12,5 +12,7 @@ public class Class1
         //Interoperability: Ensuring that software systems can communicate with one another (through APIs or middleware) is critical.
 
         // Collaboration: Building strong relationships and clear communication with partners helps align different logistical practices.
+
+        // Regulatory Alignment: Staying informed about international regulatory requirements and adapting processes to meet them.
     }
 }
