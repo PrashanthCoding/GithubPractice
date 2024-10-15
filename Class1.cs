cb024ba4fbb13185664331b98a650fca1dd831b2
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-15 18:50:42 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,12 +1,1 @@
-// Namespace definition
-namespace MyNamespace
-{
-    // Code elements (classes, structs, etc.) go here
-    // e.g., class MyClass { ... }
-}
- 
-// Using the namespace
-using MyNamespace;
- 
-// Accessing types from a namespace without importing it
-MyNamespace.TypeName myObject = new MyNamespace.TypeName();
+Hi Prashanth
